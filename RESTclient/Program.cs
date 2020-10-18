b380c1a2cc36b1a54188ef473f880b9ec7a6ae91
Marcin Tomasz Dydo <m.dydo@stud.elka.pw.edu.pl>
2020-10-18 13:57:02 +0200

    update

--- RESTclient/Program.cs
+++ RESTclient/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-
-namespace RESTclient
+using RestSharp;
+using Newtonsoft.Json;
+namespace StockClient
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new RestClient("https://stockserver20201009223011.azurewebsites.net/");
+            var request = new RestRequest("stockexchanges", DataFormat.Json);
+            var responseJson = client.Get(request);
+            string[] stockExchanges = JsonConvert.DeserializeObject<string[]>(responseJson.Content);
+            foreach (string stockExchange in stockExchanges)
+                Console.WriteLine(stockExchange);
+            Console.ReadKey();
         }
     }
 }
