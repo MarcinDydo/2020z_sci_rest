7735c2093041ca5ae77a3a088f685c0735324b5e
Marcin Tomasz Dydo <m.dydo@stud.elka.pw.edu.pl>
2020-10-19 14:50:42 +0200

    optimized autotrading

--- RESTclient/Program.cs
+++ RESTclient/Program.cs
@@ -46,12 +46,13 @@ namespace StockClient
             return companies;
         }
 
-        static void Offer(string stockExchange, string share, string buySell, double price, int amount, RestClient c)
+        static bool Offer(string stockExchange, string share, string buySell, double price, int amount, RestClient c)
         {
             var request = new RestRequest("offer", Method.POST, DataFormat.Json);
             request.AddJsonBody(new { stockExchange, share , amount, price, buySell});
             var response = c.Execute(request);
             Console.WriteLine(response.Content);
+            return (response.Content != "[]" && response.IsSuccessful);
         }
 
         static void Main(string[] args)
@@ -59,36 +60,56 @@ namespace StockClient
             var client = new RestClient("https://stockserver20201009223011.azurewebsites.net/");
             client.Authenticator = new HttpBasicAuthenticator("01149354@pw.edu.pl", "sci2020");
             UpdateInfo(client);
-            //basic transactions
             int amount = 1;
             string buy, sell; //where we buy where we sell
-            //Offer("Warszawa", "CCC", "buy", requestPrice("Warszawa", "CCC", client)[1].price, amount, client);
-            //Offer("Warszawa", "CCC", "sell", requestPrice("Warszawa", "CCC", client)[0].price, amount, client);
-            string[] vector = System.IO.File.ReadAllLines("/Users/marcin/Projects/RESTclient/RESTclient/stockExchanges.json");
-            for(int s =1; s<vector.Length;s++)
+            while (true)
             {
-                string[] A = System.IO.File.ReadAllLines("/Users/marcin/Projects/RESTclient/RESTclient/Warszawa.json");
-                string[] B = System.IO.File.ReadAllLines("/Users/marcin/Projects/RESTclient/RESTclient/" + vector[s] + ".json");
-                for (int i = 0; i < B.Length; i++)
+                string[] vector = System.IO.File.ReadAllLines("/Users/marcin/Projects/RESTclient/RESTclient/stockExchanges.json");
+                for (int s = 1; s < vector.Length; s++)
                 {
-                    if (Array.BinarySearch(A, B[i]) >= 0) {
-                        Company[] w = requestPrice("Warszawa", B[i], client);
-                        Company[] o = requestPrice(vector[s], B[i], client);
-                        if (w[1].price < o[1].price) buy = "Warszawa";
-                        else buy = vector[s];
-                        if (w[0].price > o[0].price) sell = "Warszawa";
-                        else sell = vector[s];
-                        if (!buy.Equals(sell) && Math.Max(w[0].price,o[0].price)>Math.Min(w[1].price,o[1].price))
+                    string[] A = System.IO.File.ReadAllLines("/Users/marcin/Projects/RESTclient/RESTclient/Warszawa.json");
+                    string[] B = System.IO.File.ReadAllLines("/Users/marcin/Projects/RESTclient/RESTclient/" + vector[s] + ".json");
+                    for (int i = 0; i < B.Length; i++)
+                    {
+                        if (Array.BinarySearch(A, B[i]) >= 0)
                         {
-                            Offer(buy, B[i], "buy", Math.Min(w[1].price,o[1].price), amount, client);
-                            Offer(sell, B[i], "sell", Math.Max(w[0].price, o[0].price), amount, client);
+                            Company[] w = requestPrice("Warszawa", B[i], client);
+                            Company[] o = requestPrice(vector[s], B[i], client);
+                            if (w[1].price < o[1].price)
+                            {
+                                buy = "Warszawa";
+                                amount = (int)(0.2 * w[1].amount);
+                            }
+                            else
+                            {
+                                buy = vector[s];
+                                amount = (int)(0.2 * o[1].amount);
+                            }
+                            if (w[0].price > o[0].price) sell = "Warszawa";
+                            else sell = vector[s];
+                            double commision = 0.002 * amount * Math.Max(w[0].price, o[0].price) + 0.002 * amount * Math.Min(w[1].price, o[1].price);
+                            double income = amount * (Math.Max(w[0].price, o[0].price) - Math.Min(w[1].price, o[1].price));
+                            Console.WriteLine("amount = " + amount + " income =" + income + " commision =" + commision + " profit =" + (income - commision));
+                            double buyprice = Math.Min(w[1].price, o[1].price);
+                            double sellprice = Math.Max(w[0].price, o[0].price);
+                            bool success=false;
+                            while (!buy.Equals(sell) && income - commision > 0 && !success)
+                            {
+                                if (success = Offer(buy, B[i], "buy", buyprice, amount, client)) { Console.WriteLine("bought!"); break; }
+                                buyprice = Math.Min(requestPrice("Warszawa", B[i], client)[1].price, requestPrice(vector[s], B[i], client)[1].price);
+                            }
+                            success = false;
+                            while (!buy.Equals(sell) && income - commision > 0 && !success)
+                            {
+                                if (success = Offer(sell, B[i], "sell", sellprice, amount, client)) { Console.WriteLine("sold!"); break; }
+                                sellprice = Math.Max(requestPrice("Warszawa", B[i], client)[0].price, requestPrice(vector[s], B[i], client)[0].price)
+                            }
+
                         }
-                        
                     }
                 }
+
             }
-
-
         }
 
         private static void UpdateInfo(RestClient client)
